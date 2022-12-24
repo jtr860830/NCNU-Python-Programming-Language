1e4cae8097ea13990ccfde56cd8ea6a9b375a25c
Josh Hsieh <jtr860830@gmail.com>
2022-12-24 21:28:17 +0800

    fix: week 15

--- week15/main.py
+++ week15/main.py
@@ -1,26 +1,27 @@
 from PIL import Image, ImageDraw, ImageFont
 
 with Image.open("./NCNU.jpg") as image:
+    name = "謝禹沆"
+    sn = "111213515"
+    width, height = image.size
+    draw = ImageDraw.Draw(image)
     fonts = [
         ImageFont.truetype("./font01.ttf", size=18),
         ImageFont.truetype("./font02.ttf", size=22),
     ]
-    messages = ["謝禹沆", "111213515"]
-    width, height = image.size
-    draw = ImageDraw.Draw(image)
-    _, _, w, h = draw.textbbox((0, 0), messages[0], font=fonts[0])
+    _, _, w, h = draw.textbbox((0, 0), name, font=fonts[0])
     draw.text(
         ((width - w) / 2, (height - h) / 2 + 15),
-        messages[0],
+        name,
         font=fonts[0],
         fill="black",
     )
-    _, _, w, h = draw.textbbox((0, 0), messages[1], font=fonts[1])
+    _, _, w, h = draw.textbbox((0, 0), sn, font=fonts[1])
     draw.text(
         ((width - w) / 2, (height - h) / 2 - 15),
-        messages[1],
+        sn,
         font=fonts[1],
         fill="black",
     )
 
-    image.save("111213515_謝禹沆.jpg", "JPEG")
+    image.save(f"{sn}_{name}.jpg", "JPEG")
